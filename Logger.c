663e87da811c59c96452bc1b94c94f13db4a6bd8
AdamGraser <adamgraser@interia.pl>
2014-11-20 18:58:17 +0100

    Utworzenie solucji Atmel Studio 6.2

---
+++ Logger.c
@@ -0,0 +1,17 @@
+/*
+ * Logger.c
+ *
+ * Created: 2014-11-20 18:53:19
+ *  Author: Adam
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
